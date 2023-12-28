3ab54005408ec5bcd2690b742e853ecb7a330b49
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-12-28 09:46:12 +0900

    Added ESP32H2

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ You have to change this config.
 ```
 git clone https://github.com/nopnop2002/esp-idf-benchmark
 cd esp-idf-benchmark/
-idf.py set-target {esp32/esp32s2/esp32s3/esp32c2/esp32c3/esp32c6}
+idf.py set-target {esp32/esp32s2/esp32s3/esp32c2/esp32c3/esp32c6/esp32h2}
 idf.py menuconfig
 idf.py flash monitor
 ```
--- main/main.c
+++ main/main.c
@@ -28,6 +28,8 @@ void app_main()
 	printf("Target is ESP32C3@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
 #elif CONFIG_IDF_TARGET_ESP32C6
 	printf("Target is ESP32C6@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
+#elif CONFIG_IDF_TARGET_ESP32H2
+	printf("Target is ESP32H2@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
 #endif
 
 	xTaskCreate(&dhrystone, "DHRY", 1024*4, (void *)taskHandle, my_prio+1, NULL);
